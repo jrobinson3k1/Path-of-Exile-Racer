7ecdde4363a2d3023dfd3703c8f713add662ef2a
Jason Robinson <jason.robinson@metova.com>
2015-11-28 01:05:55 -0600

    Added swipe to refresh

--- app/src/main/java/com/jasonrobinson/racer/ui/race/UpcomingFragment.java
+++ app/src/main/java/com/jasonrobinson/racer/ui/race/UpcomingFragment.java
@@ -1,6 +1,7 @@
 package com.jasonrobinson.racer.ui.race;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -10,6 +11,7 @@ import com.jasonrobinson.racer.adapter.UpcomingAdapter;
 import com.jasonrobinson.racer.dagger.ComponentHolder;
 import com.jasonrobinson.racer.network.RestService;
 import com.jasonrobinson.racer.ui.BaseFragment;
+import com.jasonrobinson.racer.ui.view.SimpleDividerDecoration;
 import com.metova.slim.annotation.Layout;
 import com.trello.rxlifecycle.FragmentEvent;
 
@@ -24,6 +26,8 @@ public class UpcomingFragment extends BaseFragment {
     @Inject
     RestService mRestService;
 
+    @Bind(R.id.swipe)
+    SwipeRefreshLayout mSwipeRefreshLayout;
     @Bind(R.id.recycler_view)
     RecyclerView mRecyclerView;
 
@@ -45,14 +49,22 @@ public class UpcomingFragment extends BaseFragment {
         super.onViewCreated(view, savedInstanceState);
         ButterKnife.bind(this, view);
 
+        mSwipeRefreshLayout.setOnRefreshListener(this::downloadRaces);
+
         mRecyclerView.setHasFixedSize(true);
 
         mLayoutManager = new LinearLayoutManager(getActivity());
         mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.addItemDecoration(new SimpleDividerDecoration(getResources()));
 
+        downloadRaces();
+    }
+
+    private void downloadRaces() {
         mRestService.races()
                 .compose(bindUntilEvent(FragmentEvent.DESTROY_VIEW))
                 .compose(uiHook())
+                .doOnTerminate(() -> mSwipeRefreshLayout.setRefreshing(false))
                 .subscribe(races -> mRecyclerView.setAdapter(new UpcomingAdapter(races)));
     }
 }
---
+++ app/src/main/java/com/jasonrobinson/racer/ui/view/SimpleDividerDecoration.java
@@ -0,0 +1,36 @@
+package com.jasonrobinson.racer.ui.view;
+
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.jasonrobinson.racer.R;
+
+public class SimpleDividerDecoration extends RecyclerView.ItemDecoration {
+
+    private Drawable mDivider;
+
+    public SimpleDividerDecoration(Resources resources) {
+        mDivider = resources.getDrawable(R.drawable.simple_divider);
+    }
+
+    public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        int left = parent.getPaddingLeft();
+        int right = parent.getWidth() - parent.getPaddingRight();
+
+        int childCount = parent.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            View child = parent.getChildAt(i);
+
+            RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
+
+            int top = child.getBottom() + params.bottomMargin;
+            int bottom = top + mDivider.getIntrinsicHeight();
+
+            mDivider.setBounds(left, top, right, bottom);
+            mDivider.draw(c);
+        }
+    }
+}
